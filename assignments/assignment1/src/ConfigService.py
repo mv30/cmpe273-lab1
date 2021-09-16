3fdc0583be95037f86a45eae3bd002213fd57367
mv30 <mayank3006verma@gmail.com>
2021-09-16 00:09:30 -0700

    mongo persistence defined

--- assignments/assignment1/src/ConfigService.py
+++ assignments/assignment1/src/ConfigService.py
@@ -1,15 +1,18 @@
 import json
 
-class MySqlToMongoConfig:
-    
-    pass
+class ConfigDetails:
+    mongo_db_name = None
+    mongo_collection_name = None
+    column_mapping = {}
 
 class ConfigService:
 
-    def __init__():
-        config_file = open('../configs/mysql_to_mongo.json')
-        config_jsons = json.load(config_file)
+    def get_table_config( table_name) -> ConfigDetails:
+        config_details = ConfigDetails()
+        file = open('./configs/mysql_to_mongo.json', 'r', encoding="ascii")
+        table_config = json.load(file.read())[table_name]
+        config_details.mongo_db_name = table_config['mongo_db_name']
+        config_details.mongo_collection_name = table_config['mongo_collection_name']
+        config_details.column_mapping = table_config['column_mapping']
+        return config_details
 
-        pass
-
-    pass
--- assignments/assignment1/src/MongoUtils.py
+++ assignments/assignment1/src/MongoUtils.py
@@ -1,6 +1,7 @@
 
 import json
-from mongopy import MongoClient
+from pymongo import MongoClient
+import DbAction_pb2
 
 class MongoService:
 
@@ -10,6 +11,24 @@ class MongoService:
     def __init__(self):
         self.connection_string = 'mongodb://localhost:27017'
         self.mongo_client = MongoClient(self.connection_string)
-        # with open
+        pass
+
+    def get_json_object( self, table_name, col_name, values_json):
+        pass
+
+    def performInsertOperation(self, db_action: DbAction_pb2.DbAction):
+
+        pass
+
+    def update( self, db_action: DbAction_pb2.DbAction):
+        if db_action.type == 'INSERT':
+            self.performInsertOperation(db_action)
+            pass
+        elif db_action.type == 'UPDATE':
+            pass
+        elif db_action.type == 'DELTE':
+            pass
+        else:
+            raise NotImplementedError(' Db action not implemented!')
         pass
 
