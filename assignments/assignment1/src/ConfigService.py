ac8a6b0645c84300b6c50b0257984e203623cba2
mv30 <mayank3006verma@gmail.com>
2021-09-16 09:38:50 -0700

    config service

--- assignments/assignment1/src/ConfigService.py
+++ assignments/assignment1/src/ConfigService.py
@@ -7,12 +7,18 @@ class ConfigDetails:
 
 class ConfigService:
 
+    @staticmethod
     def get_table_config( table_name) -> ConfigDetails:
         config_details = ConfigDetails()
         file = open('./configs/mysql_to_mongo.json', 'r', encoding="ascii")
-        table_config = json.load(file.read())[table_name]
+        file_content = file.read()
+        file.close()
+        table_config = json.loads(file_content)[table_name]
         config_details.mongo_db_name = table_config['mongo_db_name']
         config_details.mongo_collection_name = table_config['mongo_collection_name']
         config_details.column_mapping = table_config['column_mapping']
         return config_details
+
+if __name__ == '__main__':
+    print(ConfigService.get_table_config('student_temp'))
 
